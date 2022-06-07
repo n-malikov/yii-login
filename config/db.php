c9346b36142e5ef5aff93fe9ae7f328ee8e8d67a
nail-php <malikov.kzn@gmail.com>
2022-06-07 16:50:51 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=y_rbac',
+    'username' => 'vinni',
+    'password' => 'ayrS0nKL1W',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
